9a06a5f319e2b94ff17232d7ec057f9a6a743c86
Mikaela <G00401392@atu.ie>
2024-09-23 15:56:28 +0100

    Created new project

---
+++ src/main/java/ie/atu/week2/lab1recaponmicroservices/Lab1RecapOnMicroservicesApplication.java
@@ -0,0 +1,13 @@
+package ie.atu.week2.lab1recaponmicroservices;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Lab1RecapOnMicroservicesApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Lab1RecapOnMicroservicesApplication.class, args);
+    }
+
+}
---
+++ src/test/java/ie/atu/week2/lab1recaponmicroservices/Lab1RecapOnMicroservicesApplicationTests.java
@@ -0,0 +1,13 @@
+package ie.atu.week2.lab1recaponmicroservices;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Lab1RecapOnMicroservicesApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
